4ffdd929af61617601bac395969c3312c3f86974
sandermegen <sandervdberk1910@gmail.com>
2019-02-15 13:04:39 +0100

    Dit is een generated project met daarin een 16 bits

---
+++ Generated/project/Debug/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_gpio.d
@@ -0,0 +1,80 @@
+Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_gpio.o: \
+ ../Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_gpio.c \
+ C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h \
+ C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Inc/stm32f0xx_hal_conf.h \
+ C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Inc/main.h \
+ C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h \
+ C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_def.h \
+ C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
+ C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f030x8.h \
+ C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/CMSIS/Include/core_cm0.h \
+ C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h \
+ C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h \
+ C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio.h \
+ C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h \
+ C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h \
+ C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h \
+ C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h \
+ C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h \
+ C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h \
+ C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h \
+ C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h \
+ C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr.h \
+ C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h \
+ C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_spi.h \
+ C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_spi_ex.h
+
+C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h:
+
+C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Inc/stm32f0xx_hal_conf.h:
+
+C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Inc/main.h:
+
+C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h:
+
+C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_def.h:
+
+C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h:
+
+C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f030x8.h:
+
+C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/CMSIS/Include/core_cm0.h:
+
+C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
+
+C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h:
+
+C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio.h:
+
+C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h:
+
+C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h:
+
+C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h:
+
+C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h:
+
+C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h:
+
+C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h:
+
+C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h:
+
+C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h:
+
+C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr.h:
+
+C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h:
+
+C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_spi.h:
+
+C:/Users/sande/Documents/GameFPGA_Testing/Generated/project/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_spi_ex.h:
